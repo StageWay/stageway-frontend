a4204dde016f7ffa1a098b8e424419170db8e753
Fabian Brunner <Fabian.Brunner@swisscom.com>
2021-02-24 07:09:29 +0100

    fixed miner bug

--- src/app/views/stage-browse-view/stage-browse-view.component.ts
+++ src/app/views/stage-browse-view/stage-browse-view.component.ts
@@ -39,6 +39,7 @@ export class StageBrowseViewComponent implements OnInit {
 
   openStageDetailDialog(item: StageDetailModel) {
     const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = "70vw";
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
     dialogConfig.closeOnNavigation = true;
