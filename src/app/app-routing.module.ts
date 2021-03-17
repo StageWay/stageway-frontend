9e09561befc7fc3367c2f12cfed814c05de0fe89
tabbema8 <marlon.berchtold@swisscom.com>
2021-03-17 15:10:31 +0100

    Changed the name of the Dashboard and deleted the Stage-Browser-View

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { DashboardViewComponent } from './views/dashboard-view/dashboard-view.component';
 import { WelcomeViewComponent } from './views/welcome-view/welcome-view.component';
-import { StageBrowseViewComponent } from './views/stage-browse-view/stage-browse-view.component';
 import { GuideViewComponent } from "./views/guide-view/guide-view.component";
 import { CoachingViewComponent } from './views/coaching-view/coaching-view.component';
 import { AuthGuard } from '@auth0/auth0-angular';
@@ -11,9 +10,8 @@ import { AuthGuard } from '@auth0/auth0-angular';
 
 const routes: Routes = [
   { path: '', redirectTo: '/welcome', pathMatch: 'full' },
-  { path: 'dashboard', component:  DashboardViewComponent, canActivate: [AuthGuard]},
+  { path: 'stages', component:  DashboardViewComponent, canActivate: [AuthGuard]},
   { path: 'welcome', component: WelcomeViewComponent},
-  { path: 'stage', component: StageBrowseViewComponent, canActivate: [AuthGuard]},
   { path: 'guide', component: GuideViewComponent},
   { path: 'coaching', component: CoachingViewComponent},
 
@@ -29,6 +27,5 @@ const routes: Routes = [
 export class AppRoutingModule { }
 export const routingComponents = [DashboardViewComponent,
                                   WelcomeViewComponent,
-                                  StageBrowseViewComponent,
                                   GuideViewComponent,
                                   CoachingViewComponent]
