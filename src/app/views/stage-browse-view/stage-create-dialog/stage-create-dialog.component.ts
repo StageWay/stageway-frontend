dfa5ca8f1b2bdee42493d650f3c6362b09b6f148
Fabian Brunner <Fabian.Brunner@swisscom.com>
2021-04-01 07:31:34 +0200

    added functionality for multiple contact persons

--- src/app/views/stage-browse-view/stage-create-dialog/stage-create-dialog.component.ts
+++ src/app/views/stage-browse-view/stage-create-dialog/stage-create-dialog.component.ts
@@ -19,12 +19,66 @@ export class StageCreateDialogComponent implements OnInit {
 
   stageImageBackup;
   pictureSizeOkay = true;
+  contactPersons: ContactPerson[] = []
   
   constructor(@Inject(MAT_DIALOG_DATA) public stage: StageDetailModel) {
     this.stageImageBackup = stage.stageImage
+    if(typeof stage.stageContactName !== 'undefined') {
+      if(stage.stageContactName.includes(";")) {
+        var names = stage.stageContactName.split(";");
+        var emails = stage.stageContactEmail.split(";");
+        var tels = stage.stageContactTel.split(";");
+        for (let index = 0; index < names.length; index++) {
+          this.contactPersons.push({
+            name: names[index],
+            tel: tels[index],
+            email: emails[index],
+          })
+        }
+      } else {
+        this.contactPersons.push({
+          name: stage.stageContactName,
+          tel: stage.stageContactTel,
+          email: stage.stageContactEmail,
+        })
+      }
+    } else {
+      this.addContactPerson()
+    }
   }
   
   @Output() remove = new EventEmitter<FormGroup>();
+
+  addContactPerson() {
+    this.contactPersons.push({
+      name: "",
+      tel: "",
+      email: "",
+    })
+  }
+
+  removeContactPerson(person: ContactPerson) {
+    this.contactPersons.splice(this.contactPersons.indexOf(person), 1)
+    this.onUpdateContactPerson()
+  }
+
+  onUpdateContactPerson() {
+    this.stage.stageContactEmail = "";
+    this.stage.stageContactName = "";
+    this.stage.stageContactTel = "";
+    for (let index = 0; index < this.contactPersons.length; index++) {
+      const element = this.contactPersons[index];
+      if(this.contactPersons.length-1 == index){
+        this.stage.stageContactName = this.stage.stageContactName + element.name;
+        this.stage.stageContactEmail = this.stage.stageContactEmail + element.email;
+        this.stage.stageContactTel = this.stage.stageContactTel + element.tel;
+      } else {
+        this.stage.stageContactName = this.stage.stageContactName + element.name + ";";
+        this.stage.stageContactEmail = this.stage.stageContactEmail + element.email + ";";
+        this.stage.stageContactTel = this.stage.stageContactTel + element.tel + ";";
+      }
+    }
+  }
 
   ngOnInit() {
   }
@@ -48,4 +102,12 @@ export class StageCreateDialogComponent implements OnInit {
     }
   }
   
+}
+
+export interface ContactPerson {
+
+  name: string;
+  tel: string;
+  email: string;
+
 }
